e55d646d498dcf9aaa82bf340d43289fa1cb2f3e
roberto.cerquera <djrocerk@gmail.com>
2023-07-11 09:09:21 -0500

    creacion del repositorio

---
+++ src/main/java/prueba/supermercado/ORUS_SYSTEM/OrusSystemApplication.java
@@ -0,0 +1,13 @@
+package prueba.supermercado.ORUS_SYSTEM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrusSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrusSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prueba/supermercado/ORUS_SYSTEM/OrusSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.supermercado.ORUS_SYSTEM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrusSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
