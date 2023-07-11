a1a35439eae4bdfeada4ac3cd865c5e5a60b1502
JJAR140201 <avilesroajuanjose@gmail.com>
2023-07-11 09:44:54 -0500

    Creamos los modelos Frutas y Pedidos

---
+++ src/main/java/prueba/supermercado/ORUS_SYSTEM/Model/Frutas.java
@@ -0,0 +1,36 @@
+package prueba.supermercado.ORUS_SYSTEM.Model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table (name = "frutas")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Frutas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    @Column(name = "cantidad")
+    private Integer cantidad;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+    @Column(name = "fecha_creacion")
+    private Date fechaCreacion;
+
+    @Column(name = "fecha_actualizacion")
+    private Date fechaActualizacion;
+}
---
+++ src/main/java/prueba/supermercado/ORUS_SYSTEM/Model/Pedidos.java
@@ -0,0 +1,36 @@
+package prueba.supermercado.ORUS_SYSTEM.Model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "Pedidos")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pedidos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "lista_frutas")
+    private String listaFrutas;
+
+    @Column(name = "valor_total")
+    private BigDecimal valorTotal;
+
+    @Column(name = "fecha_creacion")
+    private Date fechaCreacion;
+
+    @Column(name = "fecha_actualizacion")
+    private Date fechaActualizacion;
+
+    @ManyToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Frutas fruta;
+}
